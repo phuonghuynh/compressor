c5cc3d8e7ab5fdcbb756879ce5ff718b0b3d795f
phuonghuynhbot <phuonghqhbot@gmail.com>
2016-08-02 08:23:24 +0700

    Correct the code

--- src/main/java/com/github/phuonghuynh/compressor/Sample.java
+++ src/main/java/com/github/phuonghuynh/compressor/Sample.java
@@ -1,10 +1,8 @@
 package com.github.phuonghuynh.compressor;
 
 public final class Sample {
-  protected String name = "Fred";
   
   public void func() {
-     int anInt = 243;
      System.out.println("something here");
   }
 }
